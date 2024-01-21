a71741d35f511acfbdf08206feebf8208f1b9cbd
David (BorrachoGames) <davgrego@uat.edu>
2024-01-21 16:20:28 +0900

    Animation Update
    Walk animations working. Player stops moving when keys are depressed and will face the direction he was last moving.

--- Player/player.gd
+++ Player/player.gd
@@ -1,10 +1,23 @@
 extends CharacterBody2D
 
 @export var speed: int = 100
+@onready var animations = $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+		elif velocity.y > 0: direction = "Down"
+	
+		animations.play("walk" + direction)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,3 +26,4 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
